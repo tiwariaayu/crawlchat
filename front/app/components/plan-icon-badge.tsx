9769b05cc8c0460e01b47d78f1fcdd99dc9826f2
Pramod <pramodkumar.damam73@gmail.com>
2025-09-14 07:39:39 +0530

    Fix plan badge for hobby

--- front/app/components/plan-icon-badge.tsx
+++ front/app/components/plan-icon-badge.tsx
@@ -2,7 +2,7 @@ import { TbCrown } from "react-icons/tb";
 
 export function PlanIconBadge({ planId }: { planId?: string }) {
   return (
-    ["pro", "starter"].includes(planId ?? "") && (
+    ["pro", "starter", "hobby"].includes(planId ?? "") && (
       <div className="tooltip tooltip-left" data-tip={`On ${planId} plan`}>
         <span className="badge badge-primary px-1 badge-soft">
           <TbCrown />
